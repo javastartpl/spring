9fa4ec4830758fd7982c05416666845d9f6084b8
slawek <slawekwch@gmail.com>
2021-12-28 10:47:29 +0100

    usunieta adnotacja @ConfigurationPropertiesScan

--- context-properties/src/main/java/pl/javastart/context/Main.java
+++ context-properties/src/main/java/pl/javastart/context/Main.java
@@ -2,14 +2,12 @@ package pl.javastart.context;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Scanner;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class Main {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class);
