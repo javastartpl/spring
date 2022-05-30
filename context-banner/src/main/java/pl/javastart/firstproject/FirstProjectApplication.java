1e6c58c12698a4f1c0c0c863a1ef856db0a827fb
slawek <slawekwch@gmail.com>
2022-05-30 14:01:31 +0200

    Zmiana nazw projektow z intro.* na context.*

---
+++ context-banner/src/main/java/pl/javastart/firstproject/FirstProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+	}
+
+}
---
+++ context-yaml/src/test/java/pl/javastart/firstproject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
