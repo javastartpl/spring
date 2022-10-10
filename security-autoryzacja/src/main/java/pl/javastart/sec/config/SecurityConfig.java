14b8bb8e703c5f4a667642dc67b5ee68c2ee5171
slawek <slawekwch@gmail.com>
2022-10-10 10:30:10 +0200

    Aktualizacja Spring security dla zasobow statycznych

--- security-autoryzacja/src/main/java/pl/javastart/sec/config/SecurityConfig.java
+++ security-autoryzacja/src/main/java/pl/javastart/sec/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package pl.javastart.sec.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +11,7 @@ class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests -> requests
                 .mvcMatchers("/").permitAll()
+                .mvcMatchers("/img/**", "/styles/**").permitAll()
                 .mvcMatchers("/secured").hasAnyRole("USER", "ADMIN")
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
@@ -19,13 +19,5 @@ class SecurityConfig {
         http.formLogin(login -> login.loginPage("/login").permitAll());
         http.csrf().disable();
         return http.build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().mvcMatchers(
-                "/img/**",
-                "/styles/**"
-        );
     }
 }
--- security-rejestracja/src/main/java/pl/javastart/sec/config/SecurityConfig.java
+++ security-rejestracja/src/main/java/pl/javastart/sec/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package pl.javastart.sec.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +13,7 @@ class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests -> requests
                 .mvcMatchers("/").permitAll()
+                .mvcMatchers("/img/**", "/styles/**").permitAll()
                 .mvcMatchers("/register", "/confirmation").permitAll()
                 .mvcMatchers("/secured").hasAnyRole("USER", "ADMIN")
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
@@ -22,14 +22,6 @@ class SecurityConfig {
         http.formLogin(login -> login.loginPage("/login").permitAll());
         http.csrf().disable();
         return http.build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().mvcMatchers(
-                "/img/**",
-                "/styles/**"
-        );
     }
 
     @Bean
