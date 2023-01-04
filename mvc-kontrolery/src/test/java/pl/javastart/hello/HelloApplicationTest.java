8007d68d71b9786de4a0669d9ab24d67bd6f1fdd
Slawek Ludwiczak <slawekwch@gmail.com>
2023-01-04 13:14:47 +0100

    Spring Boot 3 + Testy - MVC Kontrolery

---
+++ mvc-kontrolery/src/test/java/pl/javastart/hello/HelloApplicationTest.java
@@ -0,0 +1,13 @@
+package pl.javastart.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
