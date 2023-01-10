cd32b06c05b8feb7b8ef37986001f93babd89789
Slawek Ludwiczak <slawekwch@gmail.com>
2023-01-10 14:28:17 +0100

    Spring Boot 3 + Testy - MVC Naturalne szablony

---
+++ thymeleaf-naturalne-szablony/src/test/java/pl/javastart/urls/WebUrlsApplicationTest.java
@@ -0,0 +1,37 @@
+package pl.javastart.urls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class WebUrlsApplicationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void shouldRespondWithHttp200() throws Exception {
+        String contentAsString = mockMvc.perform(get("/"))
+                .andExpect(status().is(200))
+                .andReturn().getResponse().getContentAsString();
+        boolean containsGalleryPic = contentAsString.contains("/images/gallery.png");
+        boolean containsMockText = contentAsString.contains("Przykładowe zdjęcie 1");
+        assertAll(
+                () -> assertTrue(containsGalleryPic),
+                () -> assertFalse(containsMockText)
+        );
+
+    }
+}
