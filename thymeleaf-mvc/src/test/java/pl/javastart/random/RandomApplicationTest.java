ab9f49a72050ffac2b7fe0a1c5f964e628c7732e
Slawek Ludwiczak <slawekwch@gmail.com>
2023-01-10 09:48:07 +0100

    Spring Boot 3 + Testy - MVC Wzorzec MVC

---
+++ thymeleaf-mvc/src/test/java/pl/javastart/random/RandomApplicationTest.java
@@ -0,0 +1,13 @@
+package pl.javastart.random;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
