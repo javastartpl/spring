6f525823a8b0eba5fe628d6481ad7f33fe6361d0
Slawek Ludwiczak <slawekwch@gmail.com>
2023-01-12 13:26:11 +0100

    Spring Boot 3 + Testy - API Konfiguracja

--- api-konfiguracja/src/test/java/pl/javastart/api/ApiApplicationTests.java
+++ api-konfiguracja/src/test/java/pl/javastart/api/ApiApplicationTests.java
@@ -1,13 +1,38 @@
 package pl.javastart.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ApiApplicationTests {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void shouldReturnJsonBook() throws Exception {
+        Book book = new Book("Harry Potter", "J.K. Rowling", 1997, 223);
+        String bookJson = objectMapper.writeValueAsString(book);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/books/1"))
+                .andExpectAll(
+                        header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE),
+                        content().json(bookJson)
+                );
     }
 
 }
