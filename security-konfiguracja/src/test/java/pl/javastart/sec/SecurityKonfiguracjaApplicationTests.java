3b4a375239a53efcb5681bc03408eb53cc98fb34
Slawek Ludwiczak <slawekwch@gmail.com>
2023-01-24 10:33:01 +0100

    Spring Boot 3 + Testy - Spring Security Konfiguracja

--- security-konfiguracja/src/test/java/pl/javastart/sec/SecurityKonfiguracjaApplicationTests.java
+++ security-konfiguracja/src/test/java/pl/javastart/sec/SecurityKonfiguracjaApplicationTests.java
@@ -1,13 +1,31 @@
 package pl.javastart.sec;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SecurityKonfiguracjaApplicationTests {
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void shouldRedirectToLoginForm() throws Exception {
+		mockMvc.perform(get("/").accept(MediaType.TEXT_HTML))
+				.andExpect(status().isFound())
+				.andExpect(MockMvcResultMatchers.redirectedUrlPattern("**/login"));
+	}
 }
