c77fcbc5329855a5974c1fe9236afb14b25a56bd
Slawek Ludwiczak <slawekwch@gmail.com>
2023-01-26 11:12:21 +0100

    Spring Boot 3 + Testy - Spring Security Wylogowywanie

--- security-uwierzytelnienie/src/test/java/pl/javastart/sec/SecurityApplicationTests.java
+++ security-uwierzytelnienie/src/test/java/pl/javastart/sec/SecurityApplicationTests.java
@@ -1,13 +1,38 @@
 package pl.javastart.sec;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SecurityApplicationTests {
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void shouldRedirectToLoginForm() throws Exception {
+		mockMvc.perform(get("/").accept(MediaType.TEXT_HTML))
+				.andExpect(status().isFound())
+				.andExpect(MockMvcResultMatchers.redirectedUrlPattern("**/login"));
+	}
+
+	@Test
+	void shouldPerformLogin() throws Exception {
+		mockMvc.perform(formLogin().user("admin").password("hardPassword"))
+				.andExpect(MockMvcResultMatchers.status().isFound())
+				.andExpect(MockMvcResultMatchers.redirectedUrl("/"));
+	}
 }
--- security-wylogowanie/src/test/java/pl/javastart/sec/SecurityApplicationTests.java
+++ security-wylogowanie/src/test/java/pl/javastart/sec/SecurityApplicationTests.java
@@ -1,13 +1,35 @@
 package pl.javastart.sec;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SecurityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    MockMvc mockMvc;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void shouldLogout() throws Exception {
+        mockMvc.perform(formLogin()
+                        .user("admin")
+                        .password("hardPassword"))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/"));
+        mockMvc.perform(logout())
+                .andExpect(redirectedUrl("/login?logout"));
+    }
 }
