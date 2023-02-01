5cc1887fa6e82589269651b73d9279797d9edf1b
Slawek Ludwiczak <slawekwch@gmail.com>
2023-02-01 11:27:32 +0100

    Spring Boot 3 + Testy - Spring Security Logowanie

--- security-logowanie/src/test/java/pl/javastart/sec/SecurityApplicationTests.java
+++ security-logowanie/src/test/java/pl/javastart/sec/SecurityApplicationTests.java
@@ -1,13 +1,42 @@
 package pl.javastart.sec;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SecurityApplicationTests {
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldLogin() throws Exception {
+		mockMvc.perform(post("/login")
+						.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+						.param("username", "admin")
+						.param("password", "hardPassword"))
+				.andExpect(authenticated());
+	}
+
+	@Test
+	void shouldNotLogin() throws Exception {
+		mockMvc.perform(post("/login")
+						.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+						.param("username", "admin")
+						.param("password", "asdf"))
+				.andExpect(unauthenticated());
 	}
 
 }
