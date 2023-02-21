5d693f34b6681cb4f30a96e693ae005eb68b9cfc
Slawek Ludwiczak <slawekwch@gmail.com>
2023-02-21 12:57:26 +0100

    Dane testowe Hibernate

---
+++ data-dane-testowe-hibernate/src/main/java/pl/javastart/userbase/User.java
@@ -0,0 +1,65 @@
+package pl.javastart.userbase;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ data-dane-testowe-hibernate/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into
+    users(first_name, last_name, age)
+values
+    ('Jan', 'Kowalski', 25),
+    ('Marta', 'Jankowska', 22),
+    ('Patryk', 'Borowski', 34);
